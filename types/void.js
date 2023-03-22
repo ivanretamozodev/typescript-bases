9939b660063e7943957eb17968aac9fc9bb1389f
ivan retamozo <retamozoi445@gmail.com>
2023-03-22 16:36:24 -0300

    void

---
+++ types/void.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    const callBatman = () => {
+        //return 1
+        return;
+    };
+    const a = callBatman();
+    console.log(a);
+})();
---
+++ types/void.ts
@@ -0,0 +1,9 @@
+(() => {
+  const callBatman = (): void => {
+    //return 1
+    return;
+  };
+
+  const a = callBatman();
+  console.log(a);
+})();
